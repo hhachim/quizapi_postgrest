14756f5559015c65306390717b20290fe73ea91f
Hachim Hassani <hachim>
2025-04-19 19:36:03 +0000

    remove old frontend client

--- frontend_client/app/.next/server/pages/_app.js
+++
@@ -1,1 +0,0 @@
-(()=>{var e={};e.id=888,e.ids=[888],e.modules={4807:(e,r,t)=>{"use strict";t.r(r),t.d(r,{default:()=>App});var s=t(997);function App({Component:e,pageProps:r}){let t=e.getLayout??(e=>e);return t(s.jsx(e,{...r}))}t(108)},108:()=>{},997:e=>{"use strict";e.exports=require("react/jsx-runtime")}};var r=require("../webpack-runtime.js");r.C(e);var t=r(r.s=4807);module.exports=t})();
